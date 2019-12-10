cbf1d589dc8045aa8d52eaac3d300a90295a8e75
Dmitriy Kharlamov <dmitriy@MacBook-Pro-Dmitriy.local>
2019-12-10 21:01:51 +0300

    Enabling IAM Roles for Service Accounts on your Cluster

--- modules/kubernetes/resources.tf
+++ modules/kubernetes/resources.tf
@@ -8,3 +8,16 @@ resource "null_resource" "map_users" {
     group    = "system:masters"
   }
 }
+
+# Enabling IAM Roles for Service Accounts 
+
+resource "aws_iam_openid_connect_provider" "cluster" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = []
+  url             = module.eks.cluster_oidc_issuer_url
+}
+
+resource "aws_iam_role" "cluster" {
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  name               = var.cluster_name
+}
