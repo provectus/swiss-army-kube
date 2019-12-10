151722828c081e22778af9a91581aa92a841d3a2
Dmitriy Kharlamov <dmitriy@MacBook-Pro-Dmitriy.local>
2019-12-10 21:09:21 +0300

    Enabling IAM Roles for Service Accounts on your Cluster

--- modules/kubernetes/main.tf
+++ modules/kubernetes/main.tf
@@ -3,7 +3,28 @@ data "aws_availability_zones" "available" {}
 
 data "aws_region" "current" {}
 
-#EKS - aws kubernetes cluster
+# Enabling IAM Roles for Service Accounts 
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(module.eks.cluster_oidc_issuer_url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:aws-node"]
+    }
+
+    principals {
+      identifiers = [module.eks.cluster_iam_role_arn]
+      type        = "Federated"
+    }
+  }
+}
+
+# EKS - aws kubernetes cluster
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = ">= v7.0.0"  
