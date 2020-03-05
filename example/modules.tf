1c0e412b4bc6ebda4155797f79cb1b93ee8eb526
akastav <switch@bk.ru>
2020-03-05 08:14:01 -0500

    change module path to local

--- example/modules.tf
+++ example/modules.tf
@@ -1,5 +1,5 @@
 module "kubernetes" {
-  source = "github.com/provectus/swiss-army-kube//modules/kubernetes?ref=master"
+  source = "../modules/kubernetes"
 
   environment                = var.environment
   project                    = var.project
@@ -20,7 +20,7 @@ module "kubernetes" {
 }
 
 module "network" {
-  source = "github.com/provectus/swiss-army-kube//modules/network?ref=master"
+  source = "../modules/network"
 
   availability_zones = var.availability_zones
   environment        = var.environment
@@ -31,7 +31,7 @@ module "network" {
 
 module "system" {
   module_depends_on = [module.network.vpc_id, module.kubernetes.cluster_name]
-  source            = "github.com/provectus/swiss-army-kube//modules/system?ref=master"
+  source            = "../modules/system"
 
   environment         = var.environment
   project             = var.project
@@ -45,7 +45,7 @@ module "system" {
 # Ingress
 module "nginx" {
   module_depends_on = [module.system.kubernetes_service_account]
-  source            = "github.com/provectus/swiss-army-kube//modules/ingress/nginx?ref=master"
+  source            = "../modules/ingress/nginx"
 
   cluster_name = var.cluster_name
   domains      = var.domains
@@ -62,7 +62,7 @@ module "nginx" {
 # Monitoring
 module "prometheus" {
   module_depends_on = [module.system.kubernetes_service_account,module.nginx.nginx-ingress]
-  source            = "github.com/provectus/swiss-army-kube//modules/monitoring/prometheus?ref=master"
+  source            = "../modules/monitoring/prometheus"
 
   cluster_name = var.cluster_name
   domains      = var.domains
@@ -73,7 +73,7 @@ module "prometheus" {
 # Logging
 module "loki" {
   module_depends_on = [module.system.kubernetes_service_account,module.nginx.nginx-ingress]
-  source            = "github.com/provectus/swiss-army-kube//modules/logging/loki?ref=master"
+  source            = "../modules/logging/loki"
 
   cluster_name = var.cluster_name
   domains      = var.domains
@@ -82,7 +82,7 @@ module "loki" {
 
 #module "efk" {
 #  module_depends_on     = [module.system.kubernetes_service_account,module.nginx.nginx-ingress]
-#  source                = "github.com/provectus/swiss-army-kube//modules/logging/efk?ref=master"
+#  source                = "../modules/logging/efk"
 #  cluster_name          = var.cluster_name
 #  domains                = var.domains
 #  config_path           = "${path.module}/kubeconfig_${var.cluster_name}"
@@ -95,14 +95,14 @@ module "loki" {
 #ARGO CD
 #module "argo-cd" {
 #  module_depends_on = [module.system.kubernetes_service_account,module.nginx.nginx-ingress]
-#  source            = "github.com/provectus/swiss-army-kube//modules/cicd/argo-cd?ref=master"
+#  source            = "../modules/cicd/argo-cd"
 #
 #  domains = var.domains
 #}
 
 #module "argo-artifacts" {
 #  module_depends_on = [module.system.kubernetes_service_account,module.argo-events.argo_events_namespace,module.nginx.nginx-ingress]
-#  source            = "github.com/provectus/swiss-army-kube//modules/cicd/argo-artifacts?ref=master"
+#  source            = "../modules/cicd/argo-artifacts"
 #
 #  aws_region            = var.aws_region
 #  cluster_name          = var.cluster_name
