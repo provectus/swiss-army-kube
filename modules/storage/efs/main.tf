8a3f729de0e0d443926817d09abdb05ee1ec2d8c
RustamGimadiev <gimadiev.kzn@yandex.ru>
2020-06-05 10:35:09 +0300

    Added EFS CSI driver

---
+++ modules/storage/efs/main.tf
@@ -0,0 +1,15 @@
+resource "helm_release" "aws-efs-csi-driver" {
+  name      = "aws-efs-csi-driver"
+  chart     = "${path.module}/../../../charts/aws-efs-csi-driver"
+  namespace = "kube-system"
+}
+
+resource "aws_efs_mount_target" "this" {
+  for_each = toset(var.vpc.private_subnets)
+  file_system_id = "${aws_efs_file_system.this.id}"
+  subnet_id      = "${each.key}"
+}
+
+resource "aws_efs_file_system" "this" {
+  creation_token = var.cluster_name
+}
