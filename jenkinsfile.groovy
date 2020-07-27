6148565497e14bed5478911dd321cf501a80fa3d
akastav <switch@bk.ru>
2020-07-27 11:30:54 +0300

    fix pipeline

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -8,7 +8,7 @@ pipeline {
         wget https://releases.hashicorp.com/terraform/0.12.18/terraform_0.12.18_linux_amd64.zip
         unzip terraform_0.12.18_linux_amd64.zip
         rm terraform_0.12.18_linux_amd64.zip
-        sudo mv terraform /usr/local/bin/
+        mv terraform /usr/local/bin/
         terraform --version
         """
       }
