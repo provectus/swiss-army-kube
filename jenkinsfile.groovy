b3d8e14eba5efeec2ce37917b83c5a69b4d7fdc4
akastav <switch@bk.ru>
2020-07-27 11:44:10 +0300

    fix pipeline add tools

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -10,7 +10,7 @@ pipeline {
         sh """
         terraform --version
         cd example
-        curl -L "$(curl -Ls https://api.github.com/repos/terraform-linters/tflint/releases/latest | grep -o -E "https://.+?_linux_amd64.zip")" -o tflint.zip
+        curl -L "\$(curl -Ls https://api.github.com/repos/terraform-linters/tflint/releases/latest | grep -o -E "https://.+?_linux_amd64.zip")" -o tflint.zip
         unzip tflint.zip
         rm tflint.zip
         """
