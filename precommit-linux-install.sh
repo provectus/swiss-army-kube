0dd34fb02b253f5b05303582eaf9f9abf67fe621
akastav <switch@bk.ru>
2020-07-30 12:06:58 +0300

    fix precommit-linux-install without sudo

--- precommit-linux-install.sh
+++ precommit-linux-install.sh
@@ -1,4 +1,4 @@
-sudo apt install python3-pip gawk &&\
+apt install python3-pip gawk &&\
 pip3 install pre-commit
 curl -L "$(curl -s https://api.github.com/repos/segmentio/terraform-docs/releases/latest | grep -o -E "https://.+?-linux-amd64")" > terraform-docs && chmod +x terraform-docs && mv terraform-docs /usr/bin/
 curl -L "$(curl -s https://api.github.com/repos/terraform-linters/tflint/releases/latest | grep -o -E "https://.+?_linux_amd64.zip")" > tflint.zip && unzip tflint.zip && rm tflint.zip && mv tflint /usr/bin/
