c6930c2e3c533c6560b521367e9abc35337945ec
Nikita Dementev <ndementev@provectus.com>
2020-10-19 19:49:38 +0300

    Fix tf formatting

--- modules/logging/aws-for-fluent-bit/variables.tf
+++ modules/logging/aws-for-fluent-bit/variables.tf
@@ -5,7 +5,7 @@ variable "module_depends_on" {
 
 variable "namespace_name" {
   description = "Name of the namespace where install charts"
-  default = "logging"
+  default     = "logging"
 }
 
 variable "cluster_name" {
@@ -17,162 +17,162 @@ variable "aws_region" {
 }
 
 variable "cloudwatch_enabled" {
-  default = false
+  default     = false
   description = "Whether to enable CloudWatch output"
 }
 
 variable "cloudwatch_match" {
-  default = "'*'"
+  default     = "'*'"
   description = "The log filter"
 }
 
 variable "cloudwatch_log_group_name" {
-  default = "/aws/eks/fluentbit-cloudwatch/logs"
+  default     = "/aws/eks/fluentbit-cloudwatch/logs"
   description = "The name of the CloudWatch Log Group that you want log records sent to"
 }
 
 variable "cloudwatch_log_stream_name" {
-  default = ""
+  default     = ""
   description = "The name of the CloudWatch Log Stream that you want log records sent to."
 }
 
 variable "cloudwatch_log_stream_prefix" {
-  default = "fluentbit-"
+  default     = "fluentbit-"
   description = "Prefix for the Log Stream name"
 }
 
 variable "cloudwatch_log_key" {
-  default = ""
+  default     = ""
   description = "Log record key value of which will be sent to CloudWatch. By default whole message will be sent"
 }
 
 variable "cloudwatch_log_format" {
-  default = ""
+  default     = ""
   description = "Format of the log data"
 }
 
 variable "cloudwatch_role_arn" {
-  default = ""
+  default     = ""
   description = "ARN of an IAM role to assume (cross-account access)"
 }
 
 variable "cloudwatch_auto_create_group" {
-  default = true
+  default     = true
   description = "Whether to automatically create the log group"
 }
 
 variable "cloudwatch_endpoint" {
-  default = ""
+  default     = ""
   description = "Custom endpoint for the CloudWatch Logs API"
 }
 
 variable "cloudwatch_credentials_endpoint" {
-  default = ""
+  default     = ""
   description = "Custom HTTP endpoint to pull credentials from"
 }
 
 variable "firehose_enabled" {
-  default = false
+  default     = false
   description = "Whether to enable Firehose output"
 }
 
 variable "firehose_match" {
-  default = "'*'"
+  default     = "'*'"
   description = "The log filter"
 }
 
 variable "firehose_delivery_stream" {
-  default = ""
+  default     = ""
   description = "The name of the delivery stream that you want log records sent to"
 }
 
 variable "firehose_data_keys" {
-  default = ""
+  default     = ""
   description = "Log record key value of which will be sent to Firehose. By default whole message will be sent"
 }
 
 variable "firehose_role_arn" {
-  default = ""
+  default     = ""
   description = "ARN of an IAM role to assume (cross-account access)"
 }
 
 variable "firehose_endpoint" {
-  default = ""
+  default     = ""
   description = "Custom endpoint for the Kinesis Firehose API"
 }
 
 variable "firehose_time_key" {
-  default = false
+  default     = false
   description = "Whether to add the timestamp to the record under this key"
 }
 
 variable "firehose_time_key_format" {
-  default = ""
+  default     = ""
   description = "Strftime compliant format string for the timestamp"
 }
 
 variable "kinesis_enabled" {
-  default = false
+  default     = false
   description = "Whether to enable Kinesis output"
 }
 
 variable "kinesis_match" {
-  default = "'*'"
+  default     = "'*'"
   description = "The log filter"
 }
 
 variable "kinesis_stream" {
-  default = ""
+  default     = ""
   description = "The name of the Kinesis Data Stream that you want log records sent to"
 }
 
 variable "kinesis_partition_key" {
-  default = "container_name"
+  default     = "container_name"
   description = "A partition key is used to group data by shard within a stream"
 }
 
 variable "kinesis_append_new_line" {
-  default = false
+  default     = false
   description = "Whether to add newline after each log record"
 }
 
 variable "kinesis_data_keys" {
-  default = ""
+  default     = ""
   description = "Log record key value of which will be sent to Firehose. By default whole message will be sent"
 }
 
 variable "kinesis_role_arn" {
-  default = ""
+  default     = ""
   description = "ARN of an IAM role to assume (cross-account access)"
 }
 
 variable "kinesis_endpoint" {
-  default = ""
+  default     = ""
   description = "Custom endpoint for the Kinesis Streams API"
 }
 
 variable "kinesis_sts_endpoint" {
-  default = ""
+  default     = ""
   description = "Custom endpoint for the STS API. Used to assume your custom role provided with kinesis_role_arn variable"
 }
 
 variable "kinesis_time_key" {
-  default = false
+  default     = false
   description = "Whether to add the timestamp to the record"
 }
 
 variable "kinesis_time_key_format" {
-  default = ""
+  default     = ""
   description = "Strftime compliant format string for the timestamp"
 }
 
 variable "kinesis_compression" {
-  default = ""
+  default     = ""
   description = "Setting compression to zlib will enable zlib compression of each record"
 }
 
 variable "kinesis_aggregation" {
-  default = false
+  default     = false
   description = "Setting aggregation to true will enable KPL aggregation of records sent to Kinesis. This feature isn't compatible with the partitionKey feature"
 }
 
@@ -184,37 +184,37 @@ variable "kinesis_experimental_concurrency_retries" {
 }
 
 variable "elastic_search_enabled" {
-  default = false
+  default     = false
   description = "Whether to enable ElasticSearch output"
 }
 
 variable "elastic_match" {
-  default = "'*'"
+  default     = "'*'"
   description = "The logs filter"
 }
 
 variable "elastic_host" {
-  default = ""
+  default     = ""
   description = "The url of the Elastic Search endpoint you want log records sent to"
 }
 
 variable "elastic_aws_auth" {
-  default = "On"
+  default     = "On"
   description = "Whether to enable AWS Sigv4 Authentication for Amazon ElasticSearch Service"
 }
 
 variable "elastic_tls" {
-  default = "On"
+  default     = "On"
   description = "Whether to enable TLS support"
 }
 
 variable "elastic_port" {
-  default = ""
+  default     = ""
   description = "TCP Port of the target service"
 }
 
 variable "elastic_retry_limit" {
-  default = 1
+  default     = 1
   description = "Integer value to set the maximum number of retries allowed"
 }
 
@@ -235,16 +235,16 @@ variable "daemon_set_resources_requests_memory" {
 }
 
 variable "service_account_auto_create" {
-  default = true
+  default     = true
   description = "Whether a new service account should be created"
 }
 
 variable "service_account_name" {
-  default = "aws-for-fluent-bit"
+  default     = "aws-for-fluent-bit"
   description = "Service account name"
 }
 
 variable "service_account_annotations" {
-  default = ""
+  default     = ""
   description = "Service account annotations"
 }
