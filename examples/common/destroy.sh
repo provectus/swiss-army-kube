d85440ad41ab4558b52423cae96ac5dee7cf9065
RustamGimadiev <gimadiev.kzn@yandex.ru>
2020-11-10 14:14:30 +0300

    refactoring: added inner examples structure all examples are simple terraform code, not modules

--- examples/common/destroy.sh
+++ examples/common/destroy.sh
@@ -9,13 +9,13 @@ if [ $RESULT -ne 0 ]; then
     terraform state list | grep -e "module.*helm" | xargs terraform state rm
     terraform state list | grep -e "module.*route53" | xargs terraform state rm
 
-    terraform state rm module.nginx.kubernetes_namespace.ingress-system
-    terraform state rm module.system.kubernetes_namespace.cert-manager
+    terraform state rm module.nginx.kubernetes_namespace.ingress-system || :
+    terraform state rm module.system.kubernetes_namespace.cert-manager || :
 
-    terraform state rm module.argo-artifacts.aws_s3_bucket.argo-artifacts
+    terraform state rm module.argo-artifacts.aws_s3_bucket.argo-artifacts || :
 
-    terraform state rm module.nginx.kubernetes_secret.oauth2-proxy-secret-google
-    terraform state rm module.nginx.kubernetes_secret.oauth2-proxy-secret
+    terraform state rm module.nginx.kubernetes_secret.oauth2-proxy-secret-google || :
+    terraform state rm module.nginx.kubernetes_secret.oauth2-proxy-secret || :
 
     case "$OSTYPE" in
       darwin*)  sed -i "" 's/\"skip_final_snapshot\":.*/\"skip_final_snapshot\": true,/g' "$SCRIPTPATH/terraform.tfstate" ;;
--- examples/common/destroy_fix.sh
+++ examples/common/destroy_fix.sh
@@ -6,14 +6,14 @@ SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 terraform state list | grep -e "module.*helm" | xargs terraform state rm
 terraform state list | grep -e "module.*route53" | xargs terraform state rm
 
-terraform state rm module.nginx.kubernetes_namespace.ingress-system
-terraform state rm module.system.kubernetes_namespace.cert-manager
+terraform state rm module.nginx.kubernetes_namespace.ingress-system || :
+terraform state rm module.system.kubernetes_namespace.cert-manager || :
 
 # Workaround for non-empty s3 bucket
-terraform state rm module.argo-artifacts.aws_s3_bucket.argo-artifacts
+terraform state rm module.argo-artifacts.aws_s3_bucket.argo-artifacts || :
 
-terraform state rm module.nginx.kubernetes_secret.oauth2-proxy-secret-google
-terraform state rm module.nginx.kubernetes_secret.oauth2-proxy-secret
+terraform state rm module.nginx.kubernetes_secret.oauth2-proxy-secret-google || :
+terraform state rm module.nginx.kubernetes_secret.oauth2-proxy-secret || :
 
 # Workaround for "Error: RDS Cluster FinalSnapshotIdentifier is required when a final snapshot is required"
 case "$OSTYPE" in
