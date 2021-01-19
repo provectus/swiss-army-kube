db9fbd60cc69debb8a29f3d1f078ac58b4baba73
Gleb <takebsd@gmail.com>
2021-01-19 15:58:42 +0300

    [examples/argocd-with-applications] (#131)
    * Fix variables
    
    [examples/argocd]
    * Fix instance_type variable type
    * Fix variable type
    
    [examples/common]
    * Update for terraform 0.14
    * Fix variable description
    
    [modules/storage]
    * Remove efs storage module. It has been enabled by default
    
    ```
    kubectl get csidrivers.storage.k8s.io
    NAME              ATTACHREQUIRED   PODINFOONMOUNT   MODES        AGE
    efs.csi.aws.com   false            false            Persistent   13h
    ```
    
    [tflint]
    remove --deep option from tflint command
    tflint.hcl add deep check for aws plugin
    
    Co-authored-by: Rustam Gimadiev <gimadiev.kzn@yandex.ru>

--- examples/common/variables.tf
+++ examples/common/variables.tf
@@ -3,3 +3,33 @@ variable "region" {
   type        = string
   description = "Set default region"
 }
+
+variable "availability_zones" {
+  default     = ["eu-north-1a", "eu-north-1b"]
+  type        = list(any)
+  description = "Availability zones for project"
+}
+
+variable "environment" {
+  default     = "dev"
+  type        = string
+  description = "A value that will be used in annotations and tags to identify resources with the `Environment` key"
+}
+
+variable "project" {
+  default     = "EDUCATION"
+  type        = string
+  description = "A value that will be used in annotations and tags to identify resources with the `Project` key"
+}
+
+variable "cluster_name" {
+  default     = "swiss-army"
+  type        = string
+  description = "A name of the Amazon EKS cluster"
+}
+
+variable "mainzoneid" {
+  default     = ""
+  type        = string
+  description = "An ID of the root Route53 zone for creating sub-domains"
+}
