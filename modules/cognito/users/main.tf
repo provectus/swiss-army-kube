fb2b2b85a77651774c1def52cde92938aee92961
Karl Schriek <kschriek@gmail.com>
2021-02-02 16:23:30 +0100

    cognito users cf template now created with jsonencode

--- modules/cognito/users/main.tf
+++ modules/cognito/users/main.tf
@@ -5,43 +5,52 @@ resource "aws_cognito_user_group" "default" {
 
 
 locals {
-  //constructs a cloudformation template for cognito users
-  //TODO add dynamic group construction, currently all users are assigned to "default"
 
-  // todo: add var.tags in UserPoolTags for AWS::Cognito::UserPoolUser
-
-  cloudformation_resources = join(", \n", [
+  users = {
     for user in var.users:
-          <<EOF
-  "${user.user_hash}": {
-    "Type" : "AWS::Cognito::UserPoolUser",
-    "Properties" : {
-      "UserAttributes" : [
-        { "Name": "email", "Value": "${user.email}"},
-        { "Name": "email_verified", "Value": "true"}
-      ],
-      "Username" : "${user.email}",
-      "UserPoolId" : "${var.pool_id}"
-      }
-    },
-    "${user.user_group_hash}": {
-      "Type" : "AWS::Cognito::UserPoolUserToGroupAttachment",
-      "Properties" : {
-        "GroupName" : "${aws_cognito_user_group.default.name}",
-        "Username" : "${user.email}",
-        "UserPoolId" : "${var.pool_id}"
-      },
-      "DependsOn" : "${user.user_hash}"
-    }
-EOF
-])
+      user.user_hash => {
+        "Type" = "AWS::Cognito::UserPoolUser"
+        "Properties" = {
+          "UserAttributes" = [
+            { 
+              "Name"= "email"
+              "Value"= user.email
+            },
+            {
+              "Name"= "email_verified"
+              "Value"= "true"
+            }
+          ],
+          "Username" = user.email
+          "UserPoolId" = var.pool_id
+        }
+      }      
+  }
 
-  cloudformation_template_body = join("\n", ["{\"Resources\" : \n  {\n",local.cloudformation_resources,"}\n}"])
+  user_groups = {
+    for user in var.users:
+      user.user_group_hash => {
+        "Type" = "AWS::Cognito::UserPoolUserToGroupAttachment"
+        "Properties" = {
+          "GroupName" = aws_cognito_user_group.default.name
+          "Username" = user.email
+          "UserPoolId" = var.pool_id
+        }
+        "DependsOn" = user.user_hash
+      }
+  }
+
+  template = jsonencode({
+    "Resources" = merge(users, user_groups)
+  })
+  
+
 }
+
 
 resource "aws_cloudformation_stack" "cognito_users" {
   name = var.cloudformation_stack_name
-  template_body = local.cloudformation_template_body
+  template_body = local.template
   tags = var.tags
 }
 
