555c44b793c43109adfe529c5b16f7595220e667
Karl Schriek <kschriek@gmail.com>
2021-02-03 15:26:26 +0100

    change ingress default to "null"

---
+++ modules/kubeflow-operator/locals.tf
@@ -0,0 +1,41 @@
+locals {
+  ingress = var.ingress != null ? var.ingress : {
+    "apiVersion" = "networking.k8s.io/v1beta1"
+    "kind"       = "Ingress"
+    "metadata" = {
+      "name"        = "istio-ingress"
+      "namespace"   = "istio-system"
+      "annotations" = var.ingress_annotations
+      "labels" = {
+        "app" = "kubeflow"
+      }
+    }
+    "spec" = {
+      "tls" = [
+        {
+          "hosts" = [
+            var.domain
+          ]
+          "secretName" = "kubeflow-tls-certs"
+        }
+      ]
+      "rules" = [
+        {
+          "host" = var.domain
+          "http" = {
+            "paths" = [
+              {
+                "path" = "/*"
+                "backend" = {
+                  "serviceName" = "istio-ingressgateway"
+                  "servicePort" = 80
+                }
+              }
+            ]
+          }
+        }
+      ]
+    }
+  }
+
+}
--- modules/kubeflow-operator/variable.tf
+++ modules/kubeflow-operator/variable.tf
@@ -48,52 +48,11 @@ variable namespace {
   }
 }
 
-locals {
-  ingress = var.ingress != {} ? var.ingress : {
-    "apiVersion" = "networking.k8s.io/v1beta1"
-    "kind"       = "Ingress"
-    "metadata" = {
-      "name"        = "istio-ingress"
-      "namespace"   = "istio-system"
-      "annotations" = var.ingress_annotations
-      "labels" = {
-        "app" = "kubeflow"
-      }
-    }
-    "spec" = {
-      "tls" = [
-        {
-          "hosts" = [
-            var.domain
-          ]
-          "secretName" = "kubeflow-tls-certs"
-        }
-      ]
-      "rules" = [
-        {
-          "host" = var.domain
-          "http" = {
-            "paths" = [
-              {
-                "path" = "/*"
-                "backend" = {
-                  "serviceName" = "istio-ingressgateway"
-                  "servicePort" = 80
-                }
-              }
-            ]
-          }
-        }
-      ]
-    }
-  }
-
-}
 
 variable ingress {
   type        = any
   description = "The Ingress resource definition"
-  default     = {}
+  default = null
 }
 
 variable issuer {
