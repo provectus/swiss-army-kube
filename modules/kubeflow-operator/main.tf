dc937c981cd6c09ec02ba863a9a9711e67221b2f
Karl Schriek <kschriek@gmail.com>
2021-02-03 14:32:19 +0100

    make kubernetes resource specs variables instead of locals

--- modules/kubeflow-operator/main.tf
+++ modules/kubeflow-operator/main.tf
@@ -1,175 +1,4 @@
-locals {
-  repository = "https://github.com/kubeflow/manifests"
-  ref        = "v1.2-branch"
-  ingress = {
-    "apiVersion" = "networking.k8s.io/v1beta1"
-    "kind"       = "Ingress"
-    "metadata" = {
-      "name"        = "istio-ingress"
-      "namespace"   = "istio-system"
-      "annotations" = var.ingress_annotations
-      "labels" = {
-        "app" = "kubeflow"
-      }
-    }
-    "spec" = {
-      "tls" = [
-        {
-          "hosts" = [
-            var.domain
-          ]
-          "secretName" = "kubeflow-tls-certs"
-        }
-      ]
-      "rules" = [
-        {
-          "host" = var.domain
-          "http" = {
-            "paths" = [
-              {
-                "path" = "/*"
-                "backend" = {
-                  "serviceName" = "istio-ingressgateway"
-                  "servicePort" = 80
-                }
-              }
-            ]
-          }
-        }
-      ]
-    }
-  }
-  issuer = {
-    "apiVersion" = "cert-manager.io/v1alpha2"
-    "kind"       = "ClusterIssuer"
-    "metadata" = {
-      "name" = "kubeflow-self-signing-issuer"
-    }
-    "spec" = {
-      "selfSigned" = {}
-    }
-  }
-  namespace = {
-    "apiVersion" = "v1"
-    "kind"       = "Namespace"
-    "metadata" = {
-      "name" = "kubeflow"
-      "labels" = {
-        "control-plane"   = "kubeflow"
-        "istio-injection" = "enabled"
-      }
-    }
-  }
-  kfdef = {
-    "apiVersion" = "kfdef.apps.kubeflow.org/v1"
-    "kind"       = "KfDef"
-    "metadata" = {
-      "namespace" = "kubeflow"
-      "name"      = "kubeflow"
-    }
-    "spec" = {
-      "applications" = [
-        {
-          "kustomizeConfig" = {
-            "repoRef" = {
-              "name" = "manifests"
-              "path" = "application/v3"
-            }
-          }
-          "name" = "application"
-        },
-        {
-          "kustomizeConfig" = {
-            "repoRef" = {
-              "name" = "manifests"
-              "path" = "stacks/aws/application/istio-stack"
-            }
-          }
-          "name" = "istio-stack"
-        },
-        {
-          "kustomizeConfig" = {
-            "repoRef" = {
-              "name" = "manifests"
-              "path" = "stacks/aws/application/cluster-local-gateway"
-            }
-          }
-          "name" = "cluster-local-gateway"
-        },
-        {
-          "kustomizeConfig" = {
-            "repoRef" = {
-              "name" = "manifests"
-              "path" = "stacks/aws/application/istio"
-            }
-          }
-          "name" = "istio"
-        },
-        {
-          "kustomizeConfig" = {
-            "repoRef" = {
-              "name" = "manifests"
-              "path" = "metacontroller/base"
-            }
-          }
-          "name" = "metacontroller"
-        },
-        {
-          "kustomizeConfig" = {
-            "repoRef" = {
-              "name" = "manifests"
-              "path" = "admission-webhook/bootstrap/overlays/application"
-            }
-          }
-          "name" = "bootstrap"
-        },
-        {
-          "kustomizeConfig" = {
-            "repoRef" = {
-              "name" = "manifests"
-              "path" = "stacks/kubernetes"
-            }
-          }
-          "name" = "kubeflow-apps"
-        },
-        {
-          "kustomizeConfig" = {
-            "repoRef" = {
-              "name" = "manifests"
-              "path" = "knative/installs/generic"
-            }
-          }
-          "name" = "knative"
-        },
-        {
-          "kustomizeConfig" = {
-            "repoRef" = {
-              "name" = "manifests"
-              "path" = "kfserving/installs/generic"
-            }
-          }
-          "name" = "kfserving"
-        },
-        {
-          "kustomizeConfig" = {
-            "repoRef" = {
-              "name" = "manifests"
-              "path" = "aws/aws-istio-authz-adaptor/base_v3"
-            }
-          }
-          "name" = "aws-istio-authz-adaptor"
-        }
-      ]
-      "repos" = [
-        {
-          "name" = "manifests"
-          "uri"  = "https://github.com/kubeflow/manifests/archive/v1.2-branch.tar.gz"
-        }
-      ]
-      "version" = "v1.2-branch"
-    }
-  }
-}
+
 
 resource local_file kubeflow_operator {
   content = yamlencode({
@@ -208,22 +37,22 @@ resource local_file kubeflow_operator {
 }
 
 resource local_file namespace {
-  content  = yamlencode(local.namespace)
+  content  = yamlencode(var.namespace)
   filename = "${path.root}/${var.argocd.path}/kubeflow-namespace.yaml"
 }
 
 resource local_file kfdef {
-  content  = yamlencode(local.kfdef)
-  filename = "${path.root}/${var.argocd.path}/kfdefs/v1.1.0.yaml"
+  content  = yamlencode(var.kfdef)
+  filename = "${path.root}/${var.argocd.path}/kfdefs/kfdef.yaml"
 }
 
 resource local_file ingress {
-  content  = yamlencode(local.ingress)
+  content  = yamlencode(var.ingress)
   filename = "${path.root}/${var.argocd.path}/kfdefs/ingress.yaml"
 }
 
 resource local_file issuer {
-  content  = yamlencode(local.issuer)
+  content  = yamlencode(var.issuer)
   filename = "${path.root}/${var.argocd.path}/kfdefs/issuer.yaml"
 }
 
--- modules/kubeflow-operator/variable.tf
+++ modules/kubeflow-operator/variable.tf
@@ -19,3 +19,218 @@ variable ingress_annotations {
   description = "A set of annotations for Kubeflow Ingress"
   default     = {}
 }
+
+
+
+variable ingress_annotations {
+  type        = map(string)
+  description = "A set of annotations for Kubeflow Ingress"
+  default     = {}
+}
+
+
+variable repository {
+  type        = string
+  description = "The repository from which to roll out the Kubeflow manifests"
+  default     = "https://github.com/kubeflow/manifests"
+}
+
+variable ref {
+  type        = string
+  description = "The reference (commit/branch/tag) from which to roll out the Kubeflow manifests"
+  default     = "v1.2-branch"
+}
+
+variable namespace {
+  type        = map(string)
+  description = "The Namespace resource definition"
+  default     = {
+    "apiVersion" = "v1"
+    "kind"       = "Namespace"
+    "metadata" = {
+      "name" = "kubeflow"
+      "labels" = {
+        "control-plane"   = "kubeflow"
+        "istio-injection" = "enabled"
+      }
+    }
+  }
+}
+
+variable ingress {
+  type        = map(string)
+  description = "The Ingress resource definition"
+  default     = {
+    "apiVersion" = "networking.k8s.io/v1beta1"
+    "kind"       = "Ingress"
+    "metadata" = {
+      "name"        = "istio-ingress"
+      "namespace"   = "istio-system"
+      "annotations" = var.ingress_annotations
+      "labels" = {
+        "app" = "kubeflow"
+      }
+    }
+    "spec" = {
+      "tls" = [
+        {
+          "hosts" = [
+            var.domain
+          ]
+          "secretName" = "kubeflow-tls-certs"
+        }
+      ]
+      "rules" = [
+        {
+          "host" = var.domain
+          "http" = {
+            "paths" = [
+              {
+                "path" = "/*"
+                "backend" = {
+                  "serviceName" = "istio-ingressgateway"
+                  "servicePort" = 80
+                }
+              }
+            ]
+          }
+        }
+      ]
+    }
+  }
+}
+
+variable issuer {
+  type        = map(string)
+  description = "The Issuer resource definition"
+  default = {
+    "apiVersion" = "cert-manager.io/v1alpha2"
+    "kind"       = "ClusterIssuer"
+    "metadata" = {
+      "name" = "kubeflow-self-signing-issuer"
+    }
+    "spec" = {
+      "selfSigned" = {}
+    }
+  }
+
+variable kfdef {
+  type        = map(string)
+  description = "The KfDef resouce definition"
+  default     = {
+    "apiVersion" = "kfdef.apps.kubeflow.org/v1"
+    "kind"       = "KfDef"
+    "metadata" = {
+      "namespace" = "kubeflow"
+      "name"      = "kubeflow"
+    }
+    "spec" = {
+      "applications" = [
+        {
+          "kustomizeConfig" = {
+            "repoRef" = {
+              "name" = "manifests"
+              "path" = "application/v3"
+            }
+          }
+          "name" = "application"
+        },
+        {
+          "kustomizeConfig" = {
+            "repoRef" = {
+              "name" = "manifests"
+              "path" = "stacks/aws/application/istio-stack"
+            }
+          }
+          "name" = "istio-stack"
+        },
+        {
+          "kustomizeConfig" = {
+            "repoRef" = {
+              "name" = "manifests"
+              "path" = "stacks/aws/application/cluster-local-gateway"
+            }
+          }
+          "name" = "cluster-local-gateway"
+        },
+        {
+          "kustomizeConfig" = {
+            "repoRef" = {
+              "name" = "manifests"
+              "path" = "stacks/aws/application/istio"
+            }
+          }
+          "name" = "istio"
+        },
+        {
+          "kustomizeConfig" = {
+            "repoRef" = {
+              "name" = "manifests"
+              "path" = "metacontroller/base"
+            }
+          }
+          "name" = "metacontroller"
+        },
+        {
+          "kustomizeConfig" = {
+            "repoRef" = {
+              "name" = "manifests"
+              "path" = "admission-webhook/bootstrap/overlays/application"
+            }
+          }
+          "name" = "bootstrap"
+        },
+        {
+          "kustomizeConfig" = {
+            "repoRef" = {
+              "name" = "manifests"
+              "path" = "stacks/kubernetes"
+            }
+          }
+          "name" = "kubeflow-apps"
+        },
+        {
+          "kustomizeConfig" = {
+            "repoRef" = {
+              "name" = "manifests"
+              "path" = "knative/installs/generic"
+            }
+          }
+          "name" = "knative"
+        },
+        {
+          "kustomizeConfig" = {
+            "repoRef" = {
+              "name" = "manifests"
+              "path" = "kfserving/installs/generic"
+            }
+          }
+          "name" = "kfserving"
+        },
+        {
+          "kustomizeConfig" = {
+            "repoRef" = {
+              "name" = "manifests"
+              "path" = "aws/aws-istio-authz-adaptor/base_v3"
+            }
+          }
+          "name" = "aws-istio-authz-adaptor"
+        }
+      ]
+      "repos" = [
+        {
+          "name" = "manifests"
+          "uri"  = "https://github.com/kubeflow/manifests/archive/v1.2-branch.tar.gz"
+        }
+      ]
+      "version" = "v1.2-branch"
+    }
+  }
+}
+
+
+  
+
+
+  
+  
