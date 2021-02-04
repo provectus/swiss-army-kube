247f21852a2b2964eacaf9266bce1eed1ed030d0
Karl Schriek <kschriek@gmail.com>
2021-02-04 18:02:19 +0100

    add mlflow Application

---
+++ modules/mlflow/locals.tf
@@ -0,0 +1,129 @@
+
+locals {  
+  namespace = var.namespace != null ? var.namespace : yamlencode({
+      "apiVersion" = "v1"
+      "kind"       = "Namespace"
+      "metadata" = {
+        "name" = "mlflow"
+        "labels" = {
+          "control-plane"   = "kubeflow"
+          "istio-injection" = "enabled"
+        }
+      }
+    })
+
+
+  mlflow = var.mlflow != null ? var.mlflow : <<EOT
+  apiVersion: apps/v1
+  kind: Deployment
+  metadata:
+    name: mlflow
+    labels:
+      app: mlflow
+    namespace: mllow
+  spec:
+    replicas: 1
+    selector:
+      matchLabels:
+        app: mlflow
+    template:
+      metadata:
+        labels:
+          app: mlflow
+        annotations:
+          sidecar.istio.io/inject: "false"
+      spec:
+        containers:
+          - name: mlflow
+            securityContext: {}
+            image: "atcommons/mlflow:latest"
+            imagePullPolicy: Always
+            args:
+              - --host=0.0.0.0
+              - --port=5000
+              - --backend-store-uri=mysql://\$(RDS_USERNAME):\$(RDS_PASSWORD)@\$(RDS_HOST):\$(RDS_PORT)/mlflow
+              - --default-artifact-root=s3://\$(S3_BUCKET)/modeling/experiments
+            envFrom:         
+            - secretRef:
+                name: off-cluster-storage-secret
+            ports:
+              - name: http
+                containerPort: 5000
+                protocol: TCP
+            livenessProbe:
+              httpGet:
+                path: /
+                port: http
+            readinessProbe:
+              httpGet:
+                path: /
+                port: http
+            resources: {}
+  ---
+  apiVersion: v1
+  kind: Service
+  metadata:
+    name: mlflow
+    namespace: mlflow
+  spec:
+    selector:
+      app: mlflow
+    type: ClusterIP
+    ports:
+      - port: 80
+        targetPort: 5000
+
+  ---
+  kind: VirtualService
+  apiVersion: networking.istio.io/v1alpha3
+  metadata:
+    name: mlflow
+    namespace: kubeflow
+  spec:
+    gateways:
+      - kubeflow-gateway
+    hosts:
+      - '*'
+    http:
+      - match:
+          - uri:
+              prefix: /mlflow
+        rewrite:
+          uri: /
+        route:
+          - destination:
+              host: mlflow.mlflow.svc.cluster.local
+              port:
+                number: 80
+        timeout: 300s
+  ---
+  apiVersion: v1
+  kind: ServiceAccount
+  metadata:
+    name: mlflow
+    namespace: mlflow
+  ---
+  apiVersion: rbac.authorization.k8s.io/v1beta1
+  kind: Role
+  metadata:
+    labels:
+      app: mlflow
+    name: mlflow
+    namespace: mlflow
+  rules: []
+  ---
+  apiVersion: rbac.authorization.k8s.io/v1beta1
+  kind: RoleBinding
+  metadata:
+    labels:
+      app: mlflow
+    name: mlflow
+  roleRef:
+    apiGroup: rbac.authorization.k8s.io
+    kind: Role
+    name: mlflow
+  subjects:
+  - kind: ServiceAccount
+    name: mlflow
+  EOT
+}
