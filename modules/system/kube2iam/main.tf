aaf08a44ac0e613c3bfddca75f08fe76b6828bc9
Karl Schriek <kschriek@gmail.com>
2021-02-16 11:15:21 +0100

    add kube2iam module (YAML spec creation for argocd)

---
+++ modules/system/kube2iam/main.tf
@@ -0,0 +1,40 @@
+
+
+resource local_file kube2iam_def {
+  content = local.kube2iam_def
+  filename = "${path.root}/${var.argocd.path}/kube2iam-defs/kube2iam_def.yaml"
+}
+
+
+resource local_file kube2iam {
+  content = yamlencode({
+    "apiVersion" = "argoproj.io/v1alpha1"
+    "kind"       = "Application"
+    "metadata" = {
+      "name"      = "kube2iam"
+      "namespace" = var.argocd.namespace
+    }
+    "spec" = {
+      "destination" = {
+        "namespace" = "kube-system"
+        "server"    = "https://kubernetes.default.svc"
+      }
+      "project" = "default"
+      "source" = {
+        "repoURL"        = var.argocd.repository
+        "targetRevision" = var.argocd.branch
+        "path"           = "${var.argocd.full_path}/kube2iam-defs"
+      }
+      "syncPolicy" = {
+        "syncOptions" = [
+          "CreateNamespace=false"
+        ]
+        "automated" = {
+          "prune"    = true
+          "selfHeal" = true
+        }
+      }
+    }
+  })
+  filename = "${path.root}/${var.argocd.path}/kube2iam.yaml"
+}
