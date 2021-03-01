3f371af32188d60019c26eae5a198b359be36646
Karl Schriek <kschriek@gmail.com>
2021-03-01 18:56:04 +0100

    fix typo

--- modules/system/external-secrets/iam.tf
+++ modules/system/external-secrets/iam.tf
@@ -42,7 +42,7 @@ resource "aws_iam_role_policy" "external_secrets_access" {
                 "secretsmanager:GetResourcePolicy",
                 "secretsmanager:DescribeSecret"
             ],
-            "Resource": "arn:aws:secretsmanager:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:secret:${var.cluster_name}*"
+            "Resource": "arn:aws:secretsmanager:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:secret:${local.cluster_name}*"
         },
         {
             "Sid": "RoleAssume",
