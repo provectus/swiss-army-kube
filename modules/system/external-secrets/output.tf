efac6717af75bc335f787a0d7888e55dec1ad413
Karl Schriek <kschriek@gmail.com>
2021-03-01 19:00:44 +0100

    fix typo

--- modules/system/external-secrets/output.tf
+++ modules/system/external-secrets/output.tf
@@ -1,5 +1,5 @@
 
 output external_secrets_role_arn {
-    value = var.aws_assume_role_arn == "" ? var.aws_iam_role.this : aws_iam_role.external_secrets[0].arn
+    value = var.aws_assume_role_arn == "" ? var.aws_assume_role_arn : aws_iam_role.external_secrets[0].arn
 }
 
