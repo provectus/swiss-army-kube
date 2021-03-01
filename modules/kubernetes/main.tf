ce6c19530e6637d92617f1c5c118388922589892
Karl Schriek <kschriek@gmail.com>
2021-03-01 20:31:56 +0100

    remove manual creation of aws_iam_openid_connect_provider

--- modules/kubernetes/main.tf
+++ modules/kubernetes/main.tf
@@ -46,9 +46,9 @@ module "eks" {
   worker_groups_launch_template = concat(local.common, local.cpu, local.gpu)
 }
 
-# OIDC cluster EKS settings
-resource "aws_iam_openid_connect_provider" "cluster" {
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da2b0ab7280"]
-  url             = module.eks.cluster_oidc_issuer_url
-}
+# # OIDC cluster EKS settings
+# resource "aws_iam_openid_connect_provider" "cluster" {
+#   client_id_list  = ["sts.amazonaws.com"]
+#   thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da2b0ab7280"]
+#   url             = module.eks.cluster_oidc_issuer_url
+# }
