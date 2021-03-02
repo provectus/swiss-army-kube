6070e924e3fb1849023f4a6bc97f19ff66e53d6e
Karl Schriek <kschriek@gmail.com>
2021-03-02 16:14:09 +0100

    debugging blank external_secrets_deployment_role_arn

--- modules/system/external-secrets/locals.tf
+++ modules/system/external-secrets/locals.tf
@@ -5,7 +5,7 @@ locals {
   aws_region          = var.aws_region == "" ? data.aws_region.current.name : var.aws_region
 
   create_role = var.chart_values != "" || var.aws_assume_role_arn != "" ? true : false
-  create_full_access_policy = local.create_role && var.secret_manager_full_access  
+  create_full_access_policy = local.create_role && var.secret_manager_full_access  ? true : false
   role_policy_arns = local.create_full_access_policy ? [aws_iam_policy.this[0].arn] : []
   aws_assume_role_arn = local.create_role ? module.iam_assumable_role[0].this_iam_role_arn : var.aws_assume_role_arn
   template_helm_values = templatefile("${path.module}/values/values.yaml",
