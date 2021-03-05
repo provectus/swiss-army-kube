b2dc9b688bbf7599ddba9d82dff2c5e26b967b23
Matko Soric AT <Matko.Soric@alexanderthamm.com>
2021-03-05 12:03:20 +0100

    aws_acm_certificate

--- modules/cognito/user-pool/main.tf
+++ modules/cognito/user-pool/main.tf
@@ -50,9 +50,12 @@ locals {
   create_self_signed_acm_certificate = var.acm_arn == "" && var.self_sign_acm_certificate     
   
   //if ARN of existing certificate provided, use that. If not either create a normal ACM certificate, or create a self-signed one
-  acm_arn = var.acm_arn != "" ? var.acm_arn : (local.create_acm_certificate ? module.acm[0].this_acm_certificate_arn : aws_acm_certificate.self_signed_cert[0].arn)
+  acm_arn = var.acm_arn != "" ? var.acm_arn :
+                                              (local.create_self_signed_acm_certificate ?
+                                              aws_acm_certificate.self_signed_cert[0].arn :
+                                              module.acm[0].this_acm_certificate_arn )
 
-  depends_on = [aws_acm_certificate.self_signed_cert[0]]
+//  depends_on = [aws_acm_certificate.self_signed_cert[0]]
 }
 
 module acm {
