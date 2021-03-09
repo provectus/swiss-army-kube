e0f45ea86a00f5f2dd38a0aac77c1d12046ea7e2
Matko Soric AT <Matko.Soric@alexanderthamm.com>
2021-03-09 12:22:01 +0100

    acm_arn

--- modules/cognito/user-pool/variables.tf
+++ modules/cognito/user-pool/variables.tf
@@ -17,21 +17,13 @@ variable tags {
   default     = {}
 }
 
-variable acm_arn {
-  type = string
-  description = "The ARN of an ACM certificate to attach to the Cognito App Domain (must be in us-east-1)"
-  default = ""
-}
-
-
 variable self_sign_acm_certificate {
   type        = bool
   description = "Set to true in order to create a self-signed ACM certificates instead of letting ACM create and validate them"
   default     = false
 }
 
-
-variable loadbalancer_acm_arn {
+variable acm_arn {
   type = string
   description = "The ARN of an ACM certificate to attach to the Load Balancer"
   default = ""
