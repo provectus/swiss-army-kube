18c2448df882cb4d0e9a992ba8f33836d21d492e
Jürgen Stary <juergen.stary@alexanderthamm.com>
2021-03-15 11:18:38 +0100

    Fixed cognito mfa / poddefault to accept json

--- modules/cognito/user-pool/main.tf
+++ modules/cognito/user-pool/main.tf
@@ -63,7 +63,18 @@ resource aws_cognito_user_pool this {
     "email"
   ]
 
-  enable_username_case_sensitivity = false  
+  username_configuration {
+    case_sensitive = true
+  }
+
+  password_policy {
+    minimum_length                   = 8
+    require_lowercase                = true
+    require_numbers                  = true
+    require_symbols                  = true
+    require_uppercase                = true
+    temporary_password_validity_days = 7
+  }
 
   #TODO Jay - additions for MFA SMS
   
