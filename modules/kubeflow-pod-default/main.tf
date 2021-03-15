c12e46df0d5aba8df69ed576746e43e0b7cf4064
Jürgen Stary <juergen.stary@alexanderthamm.com>
2021-03-15 16:10:53 +0100

    add individual roles and loop over kubeflow_pod-defaults

--- modules/kubeflow-pod-default/main.tf
+++ modules/kubeflow-pod-default/main.tf
@@ -42,7 +42,7 @@ EOT
 
 
 resource local_file kubeflow_pod-defaults {
-depends_on = [ iam_assumable_role]  
+depends_on = [ module.iam_assumable_role]  
 
 for_each = {for pd in var.kubeflow_pod-defaults: pd.name => pd}
 #TODO LOOP module iam_assumable_role and inject current namespace each.value.namespace
